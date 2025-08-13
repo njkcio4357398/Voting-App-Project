4faa16156f9d219eb0c714d1c3ae36aeb90371e5
njkcio4357398 <dianalopezp15@gmail.com>
2025-08-12 20:17:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from __future__ import annotations
+import sys
+from PyQt6.QtWidgets import QApplication
+from gui import VotingWindow
 
+def main() -> None:
+    app = QApplication(sys.argv)
+    w = VotingWindow()
+    w.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
